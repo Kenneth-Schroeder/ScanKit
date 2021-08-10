fd0c9035049651d823bcbe862bcac94d781d5c8f
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2021-08-10 18:45:19 +0200

    Initial Commit

---
+++ ScanKit/ScanKit-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ScanKit-Bridging-Header.h
+//  ScanKit
+//
+//  Created by Kenneth Schröder on 10.08.21.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
