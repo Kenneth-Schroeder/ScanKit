90fc19aac8554660cbb62e1b3da7899286c04da0
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2021-10-24 16:18:37 +0200

    add error message if device doesn't support ARKit depth

--- ScanKit/AppDelegate.swift
+++ ScanKit/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ARKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if !ARWorldTrackingConfiguration.supportsFrameSemantics(.sceneDepth) {
+            // Ensure that the device supports scene depth and present
+            //  an error-message view controller, if not.
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "unsupportedDeviceMessage")
+        }
         return true
     }
 
